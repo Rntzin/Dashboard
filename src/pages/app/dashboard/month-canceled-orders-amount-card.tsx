353ee279afefae628a46e09daf539c629b4185e4
Rntzin <kinglemoyra@gmail.com>
2024-11-27 20:18:56 -0300

    feat: métricas no dashboard

--- src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
@@ -1,30 +1,32 @@
-import { getMonthOrdersAmount } from "@/api/get-month-orders-amount";
+import { getMonthCanceledOrdersAmount } from "@/api/get-month-canceled-orders-amount";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { useQuery } from "@tanstack/react-query";
-import { Boxes } from "lucide-react";
+import { Vault } from "lucide-react";
 
-export function MonthOrdersAmountCard() {
-  const { data: monthOrdersAmount } = useQuery({
-    queryFn: getMonthOrdersAmount,
-    queryKey: ["metrics", "month-orders-amount"],
+export function MonthCanceledOrdersAmountCard() {
+  const { data: monthCanceledOrdersAmount } = useQuery({
+    queryFn: getMonthCanceledOrdersAmount,
+    queryKey: ["metrics", "month-canceled-orders-amount"],
   });
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-base font-semibold">Pedidos (mês)</CardTitle>
-        <Boxes className="h-4 w-4 text-muted-foreground" />
+        <CardTitle className="text-base font-semibold">
+          Cancelados (mês)
+        </CardTitle>
+        <Vault className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {monthOrdersAmount && (
+        {monthCanceledOrdersAmount && (
           <>
             <span className="text-2xl font-bold tracking-tight">
-              {monthOrdersAmount.amount.toLocaleString("pt-BR")}
+              {monthCanceledOrdersAmount.amount.toLocaleString("pt-BR")}
             </span>
             <p className="text-sm text-muted-foreground">
-              {monthOrdersAmount.diffFromLastMonth >= 0 ? (
+              {monthCanceledOrdersAmount.diffFromLastMonth <= 0 ? (
                 <>
                   <span className="text-emerald-500 dark:text-emerald-400">
-                    +{monthOrdersAmount.diffFromLastMonth}%
+                    {monthCanceledOrdersAmount.diffFromLastMonth}%
                   </span>{" "}
                   em relação ao mês passado{" "}
                 </>
@@ -32,7 +34,7 @@ export function MonthOrdersAmountCard() {
                 <>
                   {" "}
                   <span className="text-rose-500 dark:text-rose-400">
-                    {monthOrdersAmount.diffFromLastMonth}%
+                    +{monthCanceledOrdersAmount.diffFromLastMonth}%
                   </span>{" "}
                   em relação ao mês passado
                 </>
